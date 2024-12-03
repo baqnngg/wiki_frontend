dd694e80aadccd0d23b3977d4e578f6c40e71872
baqnngg <il8696546@gmail.com>
2024-12-03 20:18:16 +0900

    12_3

--- src/WikiPage.js
+++ src/WikiPage.js
@@ -1,14 +1,54 @@
 import './wikipage.css'
 import { useParams } from "react-router-dom";
+import { useEffect,useState } from 'react';
 
 function WikiPage() {
     const {searchQuery} = useParams();
+    const [data,setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/anime/${searchQuery}`);
+                if (response.ok) {
+                    const myData = await response.json();
+                    setData(myData);
+                } else {
+                    const errorMessage = await response.json()
+                    setError(errorMessage.message);
+                }
+            } catch (error) {
+                setError("데이터를 가져오는 중 오류가 발생했습니다.");
+            };
+        };
+        fetchData();
+    }, [searchQuery])
     return(
         <div className="container">
-            <h1>위키 정보가 들어갈 곳 입니다.</h1>
             <div>{searchQuery}</div>
+            <h1>위키 정보가 들어갈 곳 입니다.</h1>
             <div className="article-container">
-                <h2>내용이 없습니다.</h2>
+                {error ? (
+                    <p>{error}</p>
+                ) : data ? (
+                    <div>
+                        <h3>이름</h3>
+                        <p style={{marginBottom:10}}>{data.이름}</p>
+                        <h3>방영일</h3>
+                        <p style={{marginBottom:10}}>{data.방영일}</p>
+                        <h3>방영요일</h3>
+                        <p style={{marginBottom:10}}>{data.방영요일}</p>
+                        <h3>제작사</h3>
+                        <p style={{marginBottom:10}}>{data.제작사}</p>
+                        <h3>감독</h3>
+                        <p style={{marginBottom:10}}>{data.감독}</p>
+                        <h3>소개</h3>
+                        <p>{data.소개}</p>
+                    </div>
+                ) : (
+                    <p>데이터를 로드 중입니다.</p>
+                )}
             </div>
         </div>
     );
